340036d75a31a44137832b7901b0572503d90e32
shoya0x00 <shoya0x00@gmail.com>
2025-04-07 16:16:04 +0000

    cleanup everything except dolby

--- camera/camera.mk
+++
@@ -1,9 +0,0 @@
-# Permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product.xml
-
-# Properties
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.lens.oem_camera_package=com.sonyericsson.android.camera
-
-$(call inherit-product, vendor/sony/extra/camera/camera-vendor.mk)
